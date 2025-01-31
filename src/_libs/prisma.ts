26167b0db6494b223192daadcdc6cad4e2bd7263
gitAntarra <antarra.coding@gmail.com>
2025-01-31 10:57:23 +0700

    coba coba

--- src/_libs/prisma.ts
+++ src/_libs/prisma.ts
@@ -1,7 +1,19 @@
 import { PrismaClient } from '@prisma/client';
+
+const databaseUrl = process.env.NODE_ENV === "production" 
+  ? process.env.ALISHBA_DB 
+  : process.env.DATABASE_URL;
+
 let prisma: PrismaClient;
 if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
+  prisma = new PrismaClient({
+    datasources:{
+      db: {
+        url: databaseUrl,
+      },
+  
+    }
+  });
 } else {
   if (!global.prisma) {
     global.prisma = new PrismaClient();

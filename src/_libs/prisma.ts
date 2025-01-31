7ac98fa1eb1dad073605c6aca5e08e1a2cd11b2a
gitAntarra <antarra.coding@gmail.com>
2025-01-31 11:05:01 +0700

    ini aman

--- src/_libs/prisma.ts
+++ src/_libs/prisma.ts
@@ -1,19 +1,7 @@
 import { PrismaClient } from '@prisma/client';
-
-const databaseUrl = process.env.NODE_ENV === "production" 
-  ? process.env.ALISHBA_DB 
-  : process.env.DATABASE_URL;
-
 let prisma: PrismaClient;
 if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient({
-    datasources:{
-      db: {
-        url: databaseUrl,
-      },
-  
-    }
-  });
+  prisma = new PrismaClient();
 } else {
   if (!global.prisma) {
     global.prisma = new PrismaClient();
